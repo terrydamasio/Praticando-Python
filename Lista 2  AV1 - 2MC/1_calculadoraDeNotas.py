57cf881766fb993a4f845306e7e1b7ef6b2fbf44
Terry Damasio <terrydamasio.dev@gmail.com>
2023-10-06 11:48:05 -0300

    Atualização pastas

--- Lista 2  AV1 - 2MC/1_calculadoraDeNotas.py
+++ Lista 2  AV1 - 2MC/1_calculadoraDeNotas.py
@@ -8,13 +8,13 @@ d = float(input("Digite a quarta nota: "))
 media = (a + b + c + d)/4
 
 if (media >= 9):
-    print("\nSua media foi: A+ ", media, ".\nParabéns, você foi aprovado!")
+    print("\nSua media foi: A+ ", media, "\nParabéns, você foi aprovado!")
 elif (media >= 7 and media <= 8.9):
-    print("\nSua media foi: B ", media, ".\nParabéns, você foi aprovado!")
+    print("\nSua media foi: B ", media, "\nParabéns, você foi aprovado!")
 elif (media >= 5 and media <= 6.9):
-    print("\nSua media foi: C ", media, ".\nEstude mais, você foi reprovado!")
+    print("\nSua media foi: C ", media, "\nEstude mais, você foi reprovado!")
 elif (media >= 3 and media <= 4.9):
-    print("\nSua media foi: D ", media, ".\nEstude mais, você foi reprovado!")
+    print("\nSua media foi: D ", media, "\nEstude mais, você foi reprovado!")
 elif (media >= 0 and media <= 2.9):
-    print("\nSua media foi: F ", media, ".\nEstude mais, você foi reprovado!")   
+    print("\nSua media foi: F ", media, "\nEstude mais, você foi reprovado!")   
 
