0d6e3c846efe7d268aab70f0abc425fabe4e0fcd
Terry Damasio <terrydamasio610@gmail.com>
2024-05-02 20:45:29 -0300

    commit

---
+++ [01] Python Basico/10_tratamento de erros/erros.py
@@ -0,0 +1,10 @@
+try: # tente
+    a = int(input("Digite um número: "))
+except (ValueError, TypeError): # excessão
+    print(f"Tivemos um problema com os tipos de dados.")
+except Exception as error:
+    print(f"Error: {error.__class__}")
+else: # se não
+    print(f"O resultado é {a}")
+finally: # finalmente
+    print("Volte sempre!")
